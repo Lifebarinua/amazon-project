da9e9750b91491a484f2a7b28d73b3809772d3e2
Lifebarinua <barinualife@gmail.com>
2025-06-12 15:36:16 +0100

    Make cart quantity interactive

--- script/amazon.js
+++ script/amazon.js
@@ -75,6 +75,11 @@ let productsHTML = '';
         });
       }
 
-    console.log(cart);
-    });
+      let cartQuantity = 0;
+
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });

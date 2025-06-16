a04d573792456a9188ae5ebd22f2eb3259e3487b
Lifebarinua <barinualife@gmail.com>
2025-06-17 00:03:53 +0100

    remove dayjs script from checkout.html, add deliveryOptionId to cart.js, create deliveryoptions.js and write some code, import function hello and dayjs into chechout.js, export formatCurrency by default in money.js

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,12 @@ if (!cart || !Array.isArray(cart)) {
     {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 2,
+      deliveryOptionId: '1'
     },
     {
       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
       quantity: 1,
+       deliveryOptionId: '2'
     },
   ];
   saveToStorage();
@@ -27,6 +29,7 @@ export function addToCart(productId, selectedQuantity) {
     cart.push({
       productId: productId,
       quantity: selectedQuantity,
+       deliveryOptionId: '1'
     });
   }
   saveToStorage();
--- script/utils/money.js
+++ script/utils/money.js
@@ -1,3 +1,5 @@
 export function formatCurrency(princeCents) {
 return (princeCents / 100).toFixed(2);
 }
+
+export default formatCurrency;

fd0d2ba61ee00366c66d1e7bead6d5f1f0d36e13
Lifebarinua <barinualife@gmail.com>
2025-06-17 00:31:12 +0100

    in money.js correct your miss spelt princeCents to priceCents and in deliveryOptions.js correct your miss spelt deliveyDays that resulted to invilid date for free shipping to the correctly spelt deliveryDays

--- script/utils/money.js
+++ script/utils/money.js
@@ -1,5 +1,5 @@
-export function formatCurrency(princeCents) {
-return (princeCents / 100).toFixed(2);
+export function formatCurrency(priceCents) {
+return (priceCents / 100).toFixed(2);
 }
 
 export default formatCurrency;

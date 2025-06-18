d4e9c1fc743a22bff7a010203e22c32dc502a4bf
Lifebarinua <barinualife@gmail.com>
2025-06-18 21:22:07 +0100

    export renderPaymentSummary  from paymentSummary.js and import it to checkout.js from paymentSummary.js and called the function in checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -1,3 +1,6 @@
 import {renderOrderSummary} from '../script/checkout/orderSummary.js'
+import {renderPaymentSummary} from './checkout/paymentSummary.js'
+
 
 renderOrderSummary();
+renderPaymentSummary();

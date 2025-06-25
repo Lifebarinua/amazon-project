e4685f1f023115f9f65476c3c316b3e35d3ee3b3
Lifebarinua <barinualife@gmail.com>
2025-06-25 15:46:03 +0100

    Create automated tests for orderSummary

--- data/cart.js
+++ data/cart.js
@@ -7,8 +7,7 @@ export function loadFromStorage() {
   cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart || !Array.isArray(cart)) {
-  cart = [
-    {
+  cart = [{
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 2,
       deliveryOptionId: '1'
--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -36,74 +36,77 @@ export function renderOrderSummary() {
     const dateString = deliveryDate.format('dddd, MMMM D');
 
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-        <div class="delivery-date">
-          Delivery date: ${dateString}
-        </div>
+<div class="cart-item-container 
+js-cart-item-container
+js-cart-item-container-${matchingProduct.id}">
+  <div class="delivery-date">
+    Delivery date: ${dateString}
+  </div>
 
-        <div class="cart-item-details-grid">
-          <img class="product-image" src="${matchingProduct.image}">
+  <div class="cart-item-details-grid">
+    <img class="product-image" src="${matchingProduct.image}">
 
-          <div class="cart-item-details">
-            <div class="product-name">
-              ${matchingProduct.name}
-            </div>
-            <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
-            </div>
-            <div class="product-quantity">
-              <span>
-                Quantity:
-                <select class="js-checkout-quantity-select" data-product-id="${matchingProduct.id}">
-                  ${Array.from({ length: 10 }, (_, i) => `
-                    <option value="${i + 1}" ${cartItem.quantity === i + 1 ? 'selected' : ''}>
-                      ${i + 1}
-                    </option>`).join('')}
-                </select>
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
-            </div>
-          </div>
-
-          <div class="delivery-options">
-            <div class="delivery-options-title">
-              Choose a delivery option:
-            </div>
-            ${deliveryOptionsHTML(matchingProduct, cartItem)}
-          </div>
-        </div>
+    <div class="cart-item-details">
+      <div class="product-name">
+        ${matchingProduct.name}
       </div>
-    `;
-  });
+      <div class="product-price">
+        $${formatCurrency(matchingProduct.priceCents)}
+      </div>
+      <div class="product-quantity
+      js-product-quantity-${matchingProduct.id}">
+        <span>
+          Quantity:
+          <select class="js-checkout-quantity-select" data-product-id="${matchingProduct.id}">
+            ${Array.from({ length: 10 }, (_, i) => `
+              <option value="${i + 1}" ${cartItem.quantity === i + 1 ? 'selected' : ''}>
+                ${i + 1}
+              </option>`).join('')}
+          </select>
+        </span>
+        <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
+          Delete
+        </span>
+      </div>
+    </div>
 
-  function deliveryOptionsHTML(matchingProduct, cartItem) {
-    let html = '';
+    <div class="delivery-options">
+      <div class="delivery-options-title">
+        Choose a delivery option:
+      </div>
+      ${deliveryOptionsHTML(matchingProduct, cartItem)}
+    </div>
+  </div>
+</div>
+`;
+});
 
-    deliveryOptions.forEach((deliveryOption) => {
+function deliveryOptionsHTML(matchingProduct, cartItem) {
+let html = '';
 
-      const dateString = calculateDeliveryDate(deliveryOption);
+deliveryOptions.forEach((deliveryOption) => {
 
-      const priceString = deliveryOption.priceCents === 0
-        ? 'FREE'
-        : `$${formatCurrency(deliveryOption.priceCents)}`;
+const dateString = calculateDeliveryDate(deliveryOption);
 
-      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+const priceString = deliveryOption.priceCents === 0
+  ? 'FREE'
+  : `$${formatCurrency(deliveryOption.priceCents)}`;
 
-      html += `
-        <div class="delivery-option js-delivery-option"
-          data-product-id="${matchingProduct.id}"
-          data-delivery-option-id="${deliveryOption.id}">
-          <input type="radio" 
-            ${isChecked ? 'checked' : ''}
-            class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-          <div> 
-            <div class="delivery-option-date">${dateString}</div>
-            <div class="delivery-option-price">${priceString} - Shipping</div>
-          </div>
-        </div>
-      `;
+const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+html += `
+  <div class="delivery-option js-delivery-option"
+    data-product-id="${matchingProduct.id}"
+    data-delivery-option-id="${deliveryOption.id}">
+    <input type="radio" 
+      ${isChecked ? 'checked' : ''}
+      class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+    <div> 
+      <div class="delivery-option-date">${dateString}</div>
+      <div class="delivery-option-price">${priceString} - Shipping</div>
+    </div>
+  </div>
+`;
     });
 
     return html;

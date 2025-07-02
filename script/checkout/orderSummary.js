7c72d355ee8a23449f55f635cac61e1a4c3d6b3e
Lifebarinua <barinualife@gmail.com>
2025-07-02 21:01:24 +0100

    Convert product into classes

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -50,7 +50,7 @@ js-cart-item-container-${matchingProduct.id}">
         ${matchingProduct.name}
       </div>
       <div class="product-price js-product-price-${matchingProduct.id}">
-        $${formatCurrency(matchingProduct.priceCents)}
+        ${matchingProduct.getPrice()}
       </div>
       <div class="product-quantity
       js-product-quantity-${matchingProduct.id}">
